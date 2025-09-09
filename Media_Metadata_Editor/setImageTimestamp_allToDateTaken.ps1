7b535fe665395991fcca6e4ffdc93e031f8f92f9
SAOOD <saood-aslam@outlook.com>
2025-09-09 16:17:46 +0100

    addingexiftool and the scripts to media edit metadata folder

---
+++ Media_Metadata_Editor/setImageTimestamp_allToDateTaken.ps1
@@ -0,0 +1,43 @@
+# ---- Configuration (your paths) ----
+$ExifToolPath = 'C:\Users\saood\PortablePrograms\ExifTool\exiftool.exe' #TODO: set this to the exiftool.exe path
+$RootFolder   = 'C:\Users\saood\Desktop\Nikkah Photos\Nikkah Photos DNG' #TODO: set this to the folder that contains the images
+
+$Extensions = @('jpg', 'jpeg', 'dng', 'cr2', 'nef', 'arw', 'orf', 'raf', 'heic', 'heif', 'mp4', 'mov')
+$extArgs = @()
+foreach ($ext in $Extensions) { $extArgs += @('-ext', $ext) }
+# Source timestamp priority (first one that exists wins), using ExifTool's redirection with ${a;b;c} to try each in order.
+$SRC = '${DateTimeOriginal\#;CreateDate\#;QuickTime:CreateDate\#;QuickTime:MediaCreateDate\#;QuickTime:TrackCreateDate\#;XMP:CreateDate\#}'
+$tagArgs = @(
+    "-AllDates<$SRC",
+    "-EXIF:DateTimeOriginal<$SRC",
+    "-XMP:CreateDate<$SRC",
+    "-XMP:ModifyDate<$SRC",
+    "-XMP:MetadataDate<$SRC",
+    "-IPTC:DateCreated<$SRC",
+    "-IPTC:TimeCreated<$SRC",
+    "-CreateDate<$SRC",
+    "-ModifyDate<$SRC",
+    "-TrackCreateDate<$SRC",
+    "-TrackModifyDate<$SRC",
+    "-MediaCreateDate<$SRC",
+    "-MediaModifyDate<$SRC",
+    "-Keys:CreationDate<$SRC",
+    "-QuickTime:ContentCreateDate<$SRC",
+    "-QuickTime:CreationDate<$SRC",
+    "-QuickTime:DateTimeOriginal<$SRC",
+    '-EXIF:OffsetTime<OffsetTime',
+    '-EXIF:OffsetTimeOriginal<OffsetTimeOriginal',
+    '-EXIF:OffsetTimeDigitized<OffsetTimeDigitized',
+    "-FileCreateDate<$SRC",
+    "-FileModifyDate<$SRC"
+)
+$runArgs = @(
+    '-r', # Recurse into subfolders
+    '-overwrite_original', # Do not keep _original backups
+    '-m', # Ignore minor warnings
+    '-progress' # Show progress
+) + $extArgs + $tagArgs + @($RootFolder)
+# Execute
+Write-Host "Running ExifTool to copy 'Date Taken' into all date tags under:`n$RootFolder`n" -ForegroundColor Cyan
+& $ExifToolPath @runArgs
+Write-Host "`nDone. Check the output above for the number of files updated." -ForegroundColor Green
---
+++ Media_Metadata_Editor/setImageTimestamp_toFixed.ps1
@@ -0,0 +1,45 @@
+# ---- Configuration (your paths) ----
+$ExifToolPath = 'C:\Users\saood\PortablePrograms\ExifTool\exiftool.exe' #TODO: set this to the exiftool.exe path
+$RootFolder   = 'C:\Users\saood\Desktop\Nikkah Photos\Nikkah Photos DNG' #TODO: set this to the folder that contains the images
+$TargetDate = '2001:01:01' #TODO: set this to the target date you want the images to be set to
+$TargetTime = '12:00:00' #TODO: set this to the target time you want the images to be set to
+$Target = "$TargetDate $TargetTime"
+
+$Extensions = @('jpg', 'jpeg', 'dng', 'cr2', 'nef', 'arw', 'orf', 'raf', 'heic', 'heif', 'mp4', 'mov')
+$extArgs = @()
+foreach ($ext in $Extensions) { $extArgs += @('-ext', $ext) }
+$tagArgs = @(
+    "-AllDates=$Target",
+    "-EXIF:DateTimeOriginal=$Target",
+    "-XMP:CreateDate=$Target",
+    "-XMP:ModifyDate=$Target",
+    "-XMP:MetadataDate=$Target",
+    "-IPTC:DateCreated=$TargetDate",
+    "-IPTC:TimeCreated=$TargetTime",
+    "-CreateDate=$Target",
+    "-ModifyDate=$Target",
+    "-TrackCreateDate=$Target",
+    "-TrackModifyDate=$Target",
+    "-MediaCreateDate=$Target",
+    "-MediaModifyDate=$Target",
+    "-Keys:CreationDate=$Target",
+    "-QuickTime:ContentCreateDate=$Target",
+    "-QuickTime:CreationDate=$Target"
+    "-QuickTime:DateTimeOriginal=$Target"
+    '-EXIF:OffsetTime=+00:00',
+    '-EXIF:OffsetTimeOriginal=+00:00',
+    '-EXIF:OffsetTimeDigitized=+00:00',
+    "-FileCreateDate=$Target",
+    "-FileModifyDate=$Target"
+)
+$runArgs = @(
+    '-r', # Recurse into subfolders
+    '-overwrite_original', #Do not keep _original backups
+    '-m', #Ignore minor warnings that could halt processing
+    '-progress' #Show progress in console
+) + $extArgs + $tagArgs + @($RootFolder)
+
+# Execute
+Write-Host "Running ExifTool to set dates to $Target on files under:`n$RootFolder`n" -ForegroundColor Cyan
+& $ExifToolPath @runArgs
+Write-Host "`nDone. Check the output above for the number of files updated." -ForegroundColor Green
