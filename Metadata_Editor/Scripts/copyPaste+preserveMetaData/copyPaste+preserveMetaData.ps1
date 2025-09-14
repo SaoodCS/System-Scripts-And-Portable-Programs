d11dd3e88a3c90022b2ac054f98adb7d4a960ea1
SAOOD <saood-aslam@outlook.com>
2025-09-14 13:39:17 +0100

    fix robocopy ps1 script

--- Metadata_Editor/Scripts/copyPaste+preserveMetaData/copyPaste+preserveMetaData.ps1
+++ Metadata_Editor/Scripts/copyPaste+preserveMetaData/copyPaste+preserveMetaData.ps1
@@ -1,23 +1,20 @@
-$SourceFolder = 'C:\Users\saood\Desktop\Nikkah DNG'   # TODO: set folder with the files you want to copy
-$DestinationFolder = 'D:\AS DNG'                       # TODO: set folder where you want to copy the files to
+$SourceFolder = 'C:\Users\saood\Desktop\Nikkah JPG'   # TODO: set folder with the files you want to copy
+$DestinationFolder = 'C:\Users\saood\Pictures\iCloud Photos\Photos' # TODO: set folder where you want to copy the files to
 
 # Options
 $copyDataAttrTimestamps = $true   # Preserve Data, Attributes, and Timestamps
 $copyDirectoryTimestamps = $true   # Preserve Directory Timestamps
 $overwriteSameFileNames = $false  # Overwrite files with the same name?
-if ($copyDataAttrTimestamps) {
-    $options = '/COPY:DAT'
-}
-if ($copyDirectoryTimestamps) {
-    $options += ' /DCOPY:T'
-}
-if (-not $overwriteSameFileNames) {
-    $options += ' /XO'
-}
-$options += ' /R:1 /W:1'
+
+$options = @()
+if ($copyDataAttrTimestamps) { $options += '/COPY:DAT' }
+if ($copyDirectoryTimestamps) { $options += '/DCOPY:T' }
 
 # Run robocopy
-Write-Host "Running robocopy from '$SourceFolder' to '$DestinationFolder' with options: $options"
-robocopy $SourceFolder $DestinationFolder *.* $options
+Write-Host "Running robocopy from '$SourceFolder' to '$DestinationFolder' with options: $($options -join ' ')"
+
+& robocopy $SourceFolder $DestinationFolder '*.*' @options
 
 Write-Host "`nRobocopy completed."
+
+# robocopy "C:\Users\saood\Desktop\Nikkah JPG" "C:\Users\saood\iCloudDrive\Nikkah JPEG" /COPY:DAT /DCOPY:T
