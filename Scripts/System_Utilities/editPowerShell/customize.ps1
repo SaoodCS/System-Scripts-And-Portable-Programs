d1a5a1111e214e9428b1e1cdda510b9f67df25f0
SAOOD <saood-aslam@outlook.com>
2026-04-19 20:19:45 +0100

    update theme

--- Scripts/System_Utilities/editPowerShell/customize.ps1
+++ Scripts/System_Utilities/editPowerShell/customize.ps1
@@ -1,7 +1,7 @@
 # TODO: update the variables here, then run updateProfile.ps1 to add the path of this file to the PowerShell profile
 $PathTextColor = [ConsoleColor]::White #TODO: set the path's text color
 $PathBackgroundColor = [ConsoleColor]::DarkBlue #TODO: set the path's background color
-$GitBranchTextColor = [ConsoleColor]::Yellow #TODO: set the git branch's text color
+$GitBranchTextColor = [ConsoleColor]::DarkYellow #TODO: set the git branch's text color
 $ShowGitBranch = $true #TODO: set to false if you don't want to see the git branch
 
 function prompt {
