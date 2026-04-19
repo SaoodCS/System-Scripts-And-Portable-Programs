c8344e4aa90b2bd1de1505dfdf4101103fc16134
SAOOD <saood-aslam@outlook.com>
2026-04-19 19:40:40 +0100

    update styling

--- Scripts/System_Utilities/editPowerShell/customize.ps1
+++ Scripts/System_Utilities/editPowerShell/customize.ps1
@@ -1,6 +1,7 @@
 # TODO: update the variables here, then run updateProfile.ps1 to add the path of this file to the PowerShell profile
-$PathTextColor = [ConsoleColor]::White #TODO: set the path's text color
-$PathBackgroundColor = [ConsoleColor]::DarkBlue #TODO: set the path's background color
+$PathTextColor = [ConsoleColor]::Black #TODO: set the path's text color
+$PathBackgroundColor = [ConsoleColor]::Yellow #TODO: set the path's background color
+$GitBranchTextColor = [ConsoleColor]::DarkRed #TODO: set the git branch's text color
 $ShowGitBranch = $true #TODO: set to false if you don't want to see the git branch
 
 function prompt {
@@ -8,8 +9,12 @@ function prompt {
     $git = ''
     if ($ShowGitBranch) {
         $b = git branch --show-current 2>$null
-        if ($b) { $git = " > ⎇ $b" }
+        if ($b) { $git = " ⎇ $b" }
     }
-    Write-Host "PS $path$git > " -ForegroundColor $PathTextColor -BackgroundColor $PathBackgroundColor -NoNewline
+    Write-Host "PS $path >" -ForegroundColor $PathTextColor -BackgroundColor $PathBackgroundColor -NoNewline
+    if ($git) {
+        Write-Host $git -ForegroundColor $GitBranchTextColor -BackgroundColor $PathBackgroundColor -NoNewline
+    }
+    Write-Host ' > ' -ForegroundColor $PathTextColor -BackgroundColor $PathBackgroundColor -NoNewline
     return ' '
 }
